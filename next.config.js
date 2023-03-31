4eff3d5d26ee2fcf9bff9fa111db7cf26c4decd4
Jean-Baptiste Colette <28951451+Jean-baptisteColette@users.noreply.github.com>
2023-03-31 09:21:16 +0100

    Remove unnecessary dependencies

--- next.config.js
+++ next.config.js
@@ -4,12 +4,7 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   reactStrictMode: true,
   env: {
-    SITE_URI: process.env.SITE_URI,
     ENVIRONMENT: process.env.ENVIRONMENT,
-    NEXT_PUBLIC_BRANCH_TAG: process.env.NEXT_PUBLIC_BRANCH_TAG,
-    NEXT_PUBLIC_BRANCH_NAME: process.env.NEXT_PUBLIC_BRANCH_NAME,
-    NEXT_PUBLIC_RUNTIME_ENVIRONMENT_CODE:
-      process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT_CODE,
   },
   i18n,
   webpack: (config) => {
